cc1549deac6e04d7561b799f49b7dc58d778f4ca
MD. Asif Raja <48570058+as-iF-30@users.noreply.github.com>
2023-12-12 15:46:02 +0530

    Update streamlit_app.py
    added entry option

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,13 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
+    return "Thanks for adding" + new_fruit
 
-my_cur.execute("insert into FRUIT_LOAD_LIST values('HI')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
